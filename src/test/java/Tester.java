8867bd518dde19536b42f0e1e2116739f363a7ff
StephenMiner <64863697+StephenMiner@users.noreply.github.com>
2024-03-28 14:52:00 -0700

    Update Tester.java

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -12,64 +12,30 @@ import java.io.*;
 public class Tester {
 
    @Test
-   public void partA () {
-      int result = APCalendar.numberOfLeapYears(0,2000);
-      assertEquals(486,result);
-      result = APCalendar.numberOfLeapYears(1950,1960);
-      assertEquals(3,result);
-      result = APCalendar.numberOfLeapYears(1900,2020);
-      assertEquals(30,result);
-      result = APCalendar.numberOfLeapYears(2000,2021);
-      assertEquals(6,result);
+   public void surpriseTest () {
+      String result;
+      RouteCipher cipher = new RouteCipher(2,4);
+      result = cipher.encryptMessage("Surprise");
+      assertEquals("Sruirspe", result);
    }
    @Test
-   public void partB(){
-      /*
-      1999 Feb 5 5 Nov 15 1   2000 Feb 5 6 Nov 15 3
-      2001 Feb 5 1 Nov 15 4   2002 Feb 5 2 Nov 15 5
-      2003 Feb 5 3 Nov 15 6   2004 Feb 5 4 Nov 15 1
-      2005 Feb 5 6 Nov 15 2   2006 Feb 5 0 Nov 15 3
-      2007 Feb 5 1 Nov 15 4
-    */
-       int result = APCalendar.dayOfWeek(2,5,1999);
-       assertEquals(5,result);
-       result = APCalendar.dayOfWeek(11,15,1999);
-       assertEquals(1,result);
-       result = APCalendar.dayOfWeek(2,5,2000);
-       assertEquals(6,result);
-       result = APCalendar.dayOfWeek(11,15,2000);
-       assertEquals(3,result);
-       result = APCalendar.dayOfWeek(2,5,2001);
-       assertEquals(1,result);
-       result = APCalendar.dayOfWeek(11,15,2001);
-       assertEquals(4,result);
-       result = APCalendar.dayOfWeek(2,5,2002);
-       assertEquals(2,result);
-       result = APCalendar.dayOfWeek(11,15,2002);
-       assertEquals(5,result);
+   public void midnightTest(){
+      String result;
+      RouteCipher cipher = new RouteCipher(2,3);
+      result = cipher.encryptMessage("Meet at midnight");
+      assertEquals("Mte eati dmnitgAhA");
+   }
+
+   @Test
+   public void tooShort(){
+      String result;
+      RouteCipher cipher = new RouteCipher(2,4);
+      result = cipher.encryptMessage("dog");
    }
 
 
 
-    public String encryptMessage(String message, int numCols, int numRows)
-    {
-        int sLength = numCols*numRows;
-        StringBuilder out= new StringBuilder();
-        int index = 0;
-        while (index +  sLength< message.length()){
-            fillBlock(message.substring(index, index+6));
-            out.append(encryptBlock());
-            index+=sLength;
-        }
-        if (index < message.length()) {
-            fillBlock(message.substring(index));
-            out.append(encryptBlock());
-        }
 
-        //fillBlock(message);
-        return out.toString();//encryptBlock();
-        /* to be implemented in part (b) */
-    }
 
 
 }
